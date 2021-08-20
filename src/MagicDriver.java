f858a465b19da56751f58529808bfb1e2ea670c0
sofia-covarrubias <sofiacovarrubias1987@gmail.com>
2021-08-20 08:50:16 -0700

    added enemy children objects

--- src/MagicDriver.java
+++ src/MagicDriver.java
@@ -2,6 +2,10 @@ public class MagicDriver
 {
     public static void main (String[] args)
     {
+        Dragon diego = new Dragon (true, 5, 2.2);
+        Goblin gary = new Goblin (false, 1, 100);
+        Golem gabe = new Golem (true, "element");
+        // Enemy[] allEnemies = [diego, gary, gabe];
 
     } // end main method
 
